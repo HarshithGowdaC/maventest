a0040ec897cdcc8141e7d9d2644074917ef028db
harshith.gowdac <harshith.gowdac@LP-5CG0233WXY.HCLT.CORP.HCL.IN>
2021-09-03 10:56:26 +0530

    committed

---
+++ Maven/src/test/java/script/Sample.java
@@ -0,0 +1,21 @@
+package script;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void f()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\harshith.gowdac\\Downloads\\chromedriver.exe");
+		WebDriver w=new ChromeDriver();
+		w.get("https://ksrtc.in/oprs-web/login/show.do");
+		w.findElement(By.id("userName")).sendKeys("harshithgowda2301@gmail.com");
+		w.findElement(By.id("password")).sendKeys("Harsha@2301");
+		w.findElement(By.id("submitBtn")).click();
+		w.close();
+	}
+}
